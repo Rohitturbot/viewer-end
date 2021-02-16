029469b5bd8228ccadf5844ba747161dc20c928c
Rohit Yadav <rohit@turbot.com>
2021-02-16 06:24:38 +0530

    deplyment test 1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from me bb its Rohit
+          Learn React
         </a>
       </header>
     </div>
